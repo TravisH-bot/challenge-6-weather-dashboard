5315d6674fcbb628d5ee3a4c98ee2b92f85c13de
George Harrison <g.a.harrison91@gmail.com>
2023-02-22 17:19:53 -0700

    adding city search storage and history code to JS. Added Div to HTML. Added class to CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,12 @@ var state;
 var country;
 var fetchButton = document.getElementById("searchBtn");
 
+// GH added - Variables
+var citySearchBar = document.getElementById("exampleDataList");
+var parentSearch = document.getElementById("searchHistory");
+var cityCount = JSON.parse(localStorage.getItem("cities")) || [];
+var cityChoice;
+
 //temporary values for lat and lon  later to be updated by Travi's function
 var lat = "39.73";
 var lon = "-104.98";
@@ -19,6 +25,31 @@ var citySearch =
   "&appid=" +
   APIKey;
 
+// GH added -city search storage & history buttons
+
+function cityRequest() {
+  cityCount.push(citySearchBar.value);
+  localStorage.setItem("cities", JSON.stringify(cityCount));
+  cityCount = JSON.parse(localStorage.getItem("cities"));
+  parentSearch.textContent = "";
+
+  for (i = 0; i < cityCount.length; i++) {
+    var addedCity = document.createElement("button");
+    addedCity.textContent = cityCount[i];
+    addedCity.className = "previousCities";
+    parentSearch.appendChild(addedCity);
+    // console.log(addedCity);
+
+    // click event on button created with previous city search
+    addedCity.addEventListener("click", function (event) {
+      cityChoice = event.target.innerText;
+      console.log(cityChoice);
+      // addCurrentWeather();
+      // getApi();
+    });
+  }
+}
+
 fetch(citySearch)
   .then(function (response) {
     return response.json();
@@ -31,28 +62,32 @@ fetch(citySearch)
   Fetches the /weather data based on the lat and lon.
   Displays the data in the top card
 */
-function addCurrentWeather(){
+function addCurrentWeather() {
   //building the request URL
-  var requestURL = "https://api.openweathermap.org/data/2.5/weather?" +
-  "&lat=" + lat +
-  "&lon=" + lon +
-  "&appid=" + APIKey +
-  "&units=imperial";
+  var requestURL =
+    "https://api.openweathermap.org/data/2.5/weather?" +
+    "&lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
 
   //fetching the data
   fetch(requestURL)
-    .then(function(response){
+    .then(function (response) {
       return response.json();
     })
-    .then(function(data){
+    .then(function (data) {
       console.log(data);
 
       //displaying the data
-      $('#current-city').text(data.name);
-      $('#current-temp').text("Temp: " + data.main.temp);
-      $('#current-wind').text("Wind: " + data.wind.speed);
-      $('#current-humidity').text("Humidity: " + data.main.humidity);
-    })
+      $("#current-city").text(data.name);
+      $("#current-temp").text("Temp: " + data.main.temp);
+      $("#current-wind").text("Wind: " + data.wind.speed);
+      $("#current-humidity").text("Humidity: " + data.main.humidity);
+    });
 }
 
 function getApi() {
@@ -82,7 +117,8 @@ function getApi() {
     });
 }
 
-fetchButton.addEventListener("click", function(){
+fetchButton.addEventListener("click", function () {
   addCurrentWeather();
   getApi();
+  cityRequest();
 });
